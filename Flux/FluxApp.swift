71e2dea17f11eb86e116a408e38c7867c9468229
song <songyunfeifly@163.com>
2025-12-26 12:49:01 +0800

    Initial Commit

---
+++ Flux/FluxApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluxApp.swift
+//  Flux
+//
+//  Created by 宋 on 2025/12/26.
+//
+
+import SwiftUI
+
+@main
+struct FluxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
